0f7d541aa3a2fd02eac83b3a563343e93b8c0889
VuManh-07 <manhvd1507@gmail.com>
2023-10-19 23:37:01 +0700

    update db

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 const uri =
   process.env.URI_DB ||
-  "mongodb+srv://manh1507:manh1507@cluster0.hl6q5ua.mongodb.net/User?retryWrites=true&w=majority";
+  "mongodb+srv://manh1507:manh1507@cluster0.hl6q5ua.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 async function connectDB(name) {
